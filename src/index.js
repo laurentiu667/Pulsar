94f8bdebd43936aa2ea8a221454d31cba0b11847
laurentiu667 <dilionlaur144@gmail.com>
2024-02-04 17:50:43 -0500

    pulsar

---
+++ src/index.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", () => {
+    let box = document.querySelector(".box");
+
+    box.addEventListener("click", () => {
+        box.classList.toggle("active");
+    })
+
+    
+    
+});
